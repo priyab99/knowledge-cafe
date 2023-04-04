a44af2e3e84652f1ba20eac32dd6bb45544083ad
priyab99 <priya.bd.9911@gmail.com>
2023-04-04 21:41:16 +0600

    selected bookmark counting done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import './Blog.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Blog = (props) => {
     const{img,image,title,date,time,body}=props.blog;
+    const addBookMark=props.addBookMark;
     return (
         <div className='blog'>
 
 
        <img src={img} alt="" />
+    
+            <div className='intro-info'>
+        
        <div>
-       <div>
-        <img className='user-image' src={image} alt="" />
        
         <h4>{title}</h4>
-        <h5>{date}</h5>
+        <h5>{date}(days ago)</h5>
        </div>
-       <div>
-        <p>{time}</p>
+       <div className='bookmark-container'>
+        <p>{time}mins read</p>
+        <button onClick={()=>addBookMark(props.blog)} className='bookmark' ><FontAwesomeIcon icon={faBookmark} /></button>
        </div>
 
        </div>
 
-       <p>{body}</p>
+       <p className='body-style'>{body}</p>
+       <a className='anchor' href="/mark">Mark As Read</a>
 
 
             
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,17 +4,25 @@ import './Profile.css'
 
 const Profile = () => {
     const [blogs, setBlogs]=useState([])
+    const [mark,setMarks]=useState([])
     useEffect(()=>{
         fetch('fakedata.json')
         .then(res=>res.json())
         .then(data=>setBlogs(data))
     },[]);
+
+     const addBookMark=(blog)=>{
+        const newMark=[...mark,blog];
+        setMarks(newMark);
+
+     }
     return (
         <div className='profile-container'>
             <div className='blog-container'>
         {
             blogs.map(blog=> <Blog key={blog.id}
-            blog={blog}>
+            blog={blog}
+            addBookMark={addBookMark}>
                 
             </Blog> )
         }
@@ -22,7 +30,8 @@ const Profile = () => {
 
             </div>
             <div className='bookmark-container'>
-                <h2>Bookmarked</h2>
+                <h2>spend time on read</h2>
+                <h2>Bookmarked Blogs:{mark.length}</h2>
 
             </div>
             
